08c7af45c178aa4facfaef4956b37f8dac5cd402
Clark Richard Green <Clark@Clark-Green.local>
2017-04-19 16:52:06 -0600

    Dang, this is hard

--- db-tool/src/app/api.service.ts
+++ db-tool/src/app/api.service.ts
@@ -5,15 +5,17 @@ import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
+
+
 @Injectable()
 export class APIService {
 	private buildingsUrl = 'localhost:9999/buildings';
 
 	constructor (private http: Http) {}
 
-	getBuildings(): Observable<Building[]> {
-		return this.http.get(this.buildingsUrl)
-						.map(this.extractData)
-						.catch(this.handleError);
-	}
+	//	getBuildings(): Observable<Building[]> {
+		//		return this.http.get(this.buildingsUrl)
+		//				.map(this.extractData)
+		//				.catch(this.handleError);
+	//	}
 }
