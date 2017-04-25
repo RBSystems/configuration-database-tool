681f6cb3f0daf0ab2df2f96c40fce341e1afec40
Daniel Randall <dgrandall@me.com>
2017-04-25 15:42:23 -0600

    displays all config data

--- db-tool/src/app/configuration.component.ts
+++ db-tool/src/app/configuration.component.ts
@@ -6,18 +6,19 @@ import { APIService } from './api.service';
 @Component({
 	selector: 'configuration',
 	templateUrl: './configuration.component.html',
+	styleUrls: ['./configuration.component.css'],
 	providers: [APIService],
 })
 
 export class ConfigurationComponent implements OnInit {
-	configuration: Object;
-	DeviceTypes: Object;
-	PowerStates: Object;
-	Ports: Object;
-	Commands: Object;
-	Microservices: Object;
-	Endpoints: Object;
-	DeviceRoleDefinitions: Object;
+	configuration: any;
+	devicetypes: DeviceType[];
+	powerstates: Object;
+	ports: Object;
+	commands: Object;
+	microservices: Object;
+	endpoints: Object;
+	deviceroledefinitions: Object;
 
 	ngOnInit(): void {
 		this.getConfig();	
@@ -26,6 +27,71 @@ export class ConfigurationComponent implements OnInit {
 	constructor(private api: APIService) {}
 
 	getConfig(): void {
-		this.api.getConfig().subscribe(val => this.configuration = val);	
+		this.api.getConfig().subscribe(val => { 
+								this.configuration = val; 
+								this.devicetypes = this.configuration.DeviceTypes; 
+								this.powerstates = this.configuration.PowerStates;
+								this.ports = this.configuration.Ports;
+								this.commands = this.configuration.Commands;
+								this.microservices = this.configuration.Microservices;
+								this.endpoints = this.configuration.Endpoints;
+								this.deviceroledefinitions = this.configuration.DeviceRoleDefinitions;
+							});	
+	//	this.DeviceTypes = JSON.parse(this.configuration.toString()).DeviceTypes;
 	}
+}
+
+export class Configuration {
+	DeviceTypes: DeviceType[];
+	PowerStates: PowerState[];
+	Ports: Port[];
+	Commands: Command[];
+	Microservices: Microservice[];
+	Endpoints: Endpoint[];
+	DeviceRoleDefinitions: DeviceRoleDefinition[];
+}
+
+export class DeviceType {
+	id: number;
+	name: string;
+	description: string;
+}
+
+export class PowerState {
+	id: number;
+	name: string;
+	description: string;
+}
+
+export class Port {
+	id: number;
+	name: string;
+	description: string;
+}
+
+export class Command {
+	id: number;
+	name: string;
+	description: string;
+	priority: number;
+}
+
+export class Microservice {
+	id: number;
+	name: string;
+	address: string;
+	description: string;
+}
+
+export class Endpoint {
+	id: number;
+	name: string;
+	path: string;
+	description: string;
+}
+
+export class DeviceRoleDefinition {
+	id: number;
+	name: string;
+	description: string;
 }
