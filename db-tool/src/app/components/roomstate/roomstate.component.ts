62ba8a5f6960ea970d557e3aa99f29374f258b8f
CalebLarsen <caleb.john.larsen@gmail.com>
2018-10-24 08:45:21 -0600

    Caleb's test things

--- db-tool/src/app/components/roomstate/roomstate.component.ts
+++ db-tool/src/app/components/roomstate/roomstate.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { SmeeComponent } from '../smee.component';
+import { Device } from '../../objects.ts';
 
 @Component({
   selector: 'app-roomstate',
@@ -9,10 +10,16 @@ import { SmeeComponent } from '../smee.component';
 })
 export class RoomStateComponent implements OnInit, SmeeComponent {
   @Input() data;
-
+  deviceList : Device[];
+  deviceColumns : string[] = ["name", "description", "address"];
   constructor(private api: ApiService) { }
 
   ngOnInit() {
+    this.GetDeviceList();
   }
-
+  GetDeviceList() {
+    this.api.GetDeviceList(this.data._id).subscribe(val => {
+      this.deviceList = val;
+    })
+  } 
 }
---
+++ quartermaster/cache.go
@@ -0,0 +1,26 @@
+package quartermaster
+
+/*
+import "strings"
+
+type CouchCache struct {
+	address           string
+	username          string
+	password          string
+	DeviceStatusCache map[string]BuildingStatus
+}
+
+func NewCache(address, username, password string) *CouchCache {
+
+	return &CouchCache{
+		address:  strings.Trim(address, "/"),
+		username: username,
+		password: password,
+	}
+}
+
+func (c *Cache) FillDeviceStatusCache(db CouchDB) error {
+	//TODO MAKE THIS WORK
+	return nil
+}
+*/
